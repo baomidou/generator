c4efc2a91ecd1ea04c40b41bf56cbb6011e88c74
nieqiurong <nieqiurong@163.com>
2020-10-15 09:50:47 +0800

    删除非生成器相关代码.

--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/fillperformance/service/IPerformanceModelService.java
+++
@@ -1,8 +0,0 @@
-package com.baomidou.mybatisplus.test.h2.fillperformance.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.baomidou.mybatisplus.test.h2.fillperformance.model.PerformanceModel;
-
-public interface IPerformanceModelService extends IService<PerformanceModel> {
-
-}
--- mybatis-plus/src/test/java/com/baomidou/mybatisplus/test/h2/tenant/service/IStudentService.java
+++
@@ -1,8 +0,0 @@
-package com.baomidou.mybatisplus.test.h2.tenant.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.baomidou.mybatisplus.test.h2.tenant.model.Student;
-
-public interface IStudentService extends IService<Student> {
-
-}
