fcabdcf2a4c8218cdf4de0d3bd2435c3b9f81f82
lanjerry <38046851@qq.com>
2021-01-21 16:45:16 +0800

    优化测试类.

--- mybatis-plus-generator/src/test/java/com/baomidou/mybatisplus/test/generator/H2CodeGeneratorTest.java
+++ mybatis-plus-generator/src/test/java/com/baomidou/mybatisplus/test/generator/H2CodeGeneratorTest.java
@@ -1,21 +1,24 @@
 package com.baomidou.mybatisplus.test.generator;
 
-import com.baomidou.mybatisplus.annotation.IdType;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.h2.Driver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.po.LikeTable;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import org.apache.ibatis.jdbc.ScriptRunner;
-import org.h2.Driver;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.SQLException;
+import com.baomidou.mybatisplus.generator.fill.Column;
+import com.baomidou.mybatisplus.generator.fill.Property;
 
 /**
  * H2代码生成
@@ -23,12 +26,6 @@ import java.sql.SQLException;
  * @author nieqiuqiu
  */
 class H2CodeGeneratorTest {
-
-    private static final String outPutDir = System.getProperty("os.name").toLowerCase().contains("windows") ? "D://tmp" : "/tmp";
-
-    private static final DataSourceConfig DATA_SOURCE_CONFIG = new DataSourceConfig
-        .Builder("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;CASE_INSENSITIVE_IDENTIFIERS=TRUE","sa","")
-        .driver(Driver.class).build();
 
     @BeforeAll
     public static void before() throws SQLException {
@@ -40,13 +37,39 @@ class H2CodeGeneratorTest {
         conn.close();
     }
 
+    /**
+     * 数据源配置
+     */
+    private static final DataSourceConfig DATA_SOURCE_CONFIG = new DataSourceConfig
+        .Builder("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;CASE_INSENSITIVE_IDENTIFIERS=TRUE","sa","")
+        .driver(Driver.class).build();
 
-    private StrategyConfig strategyConfig() {
-        return new StrategyConfig.Builder().enableSqlFilter(true).capitalMode(true).entityBuilder().lombok(false).naming(NamingStrategy.underline_to_camel).build();
+    /**
+     * 全局配置
+     */
+    private GlobalConfig globalConfig() {
+        return new GlobalConfig.Builder()
+            .outputDir(System.getProperty("os.name").toLowerCase().contains("windows") ? "D://tmp" : "/tmp")// 输出目录
+            .author("test")// 作者
+            .openDir(false)// 是否打开输出目录
+            .fileOverride(true)// 是否覆盖已有文件
+            .build();
     }
 
-    private GlobalConfig globalConfig() {
-        return new GlobalConfig.Builder().activeRecord(false).idType(IdType.ASSIGN_ID).author("test").outputDir(outPutDir).openDir(true).fileOverride(true).build();
+    /**
+     * 策略配置
+     */
+    private StrategyConfig strategyConfig() {
+        return new StrategyConfig.Builder()
+            .enableSqlFilter(true)// 启用sql过滤
+            .capitalMode(true)// 是否大写命名
+            .entityBuilder()// 实体配置构建者
+            .lombok(false)// 是否为lombok模型
+            .versionColumnName("version") //乐观锁数据库表字段
+            .naming(NamingStrategy.underline_to_camel)// 数据库表映射到实体的命名策略
+            .addTableFills(new Column("create_time", FieldFill.INSERT))
+            .addTableFills(new Property("updateTime", FieldFill.INSERT_UPDATE))
+            .build();
     }
 
     @Test
@@ -117,8 +140,7 @@ class H2CodeGeneratorTest {
     void testSimple(){
         new AutoGenerator(DATA_SOURCE_CONFIG)
             .global(globalConfig())
-            .strategy(new StrategyConfig.Builder().capitalMode(true).addTablePrefix("t_").addInclude("t_simple")
-                .entityBuilder().versionFieldName("version").naming(NamingStrategy.underline_to_camel).build())
+            .strategy(strategyConfig().setInclude("t_simple"))
             .execute();
     }
 }
