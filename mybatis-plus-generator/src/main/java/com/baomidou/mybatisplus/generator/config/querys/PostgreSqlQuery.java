9558320ae57db28d970fc72451ab62a4c0b245f5
hubin <243194995@qq.com>
2022-06-28 18:20:33 +0800

    还原pg合并部分

--- mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/querys/PostgreSqlQuery.java
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/config/querys/PostgreSqlQuery.java
@@ -15,6 +15,11 @@
  */
 package com.baomidou.mybatisplus.generator.config.querys;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * PostgreSql 表数据查询
  *
@@ -28,48 +33,65 @@ public class PostgreSqlQuery extends AbstractDbQuery {
         return "SELECT A.tablename, obj_description(relfilenode, 'pg_class') AS comments FROM pg_tables A, pg_class B WHERE A.schemaname='%s' AND A.tablename = B.relname";
     }
 
-
     @Override
     public String tableFieldsSql() {
-        return "SELECT A.attname AS name,format_type (A.atttypid,A.atttypmod) AS type,col_description (A.attrelid,A.attnum) AS comment,\n" +
-            "(CASE WHEN (SELECT COUNT (*) FROM pg_constraint AS PC WHERE A.attnum = PC.conkey[1] AND PC.contype = 'p' AND A.attrelid = PC.conrelid) > 0 THEN 'PRI' ELSE '' END) AS key \n" +
-            "FROM pg_class AS C,pg_attribute AS A WHERE A.attrelid='\"%s\"'::regclass AND A.attrelid= C.oid AND A.attnum> 0 AND NOT A.attisdropped ORDER  BY A.attnum";
+        return "SELECT\n" +
+            "   A.attname AS name,format_type (A.atttypid,A.atttypmod) AS type,col_description (A.attrelid,A.attnum) AS comment,\n" +
+            "\t D.column_default,\n" +
+            "   CASE WHEN length(B.attname) > 0 THEN 'PRI' ELSE '' END AS key\n" +
+            "FROM\n" +
+            "   pg_attribute A\n" +
+            "LEFT JOIN (\n" +
+            "    SELECT\n" +
+            "        pg_attribute.attname\n" +
+            "    FROM\n" +
+            "        pg_index,\n" +
+            "        pg_class,\n" +
+            "        pg_attribute\n" +
+            "    WHERE\n" +
+            "        pg_class.oid ='\"%s\"' :: regclass\n" +
+            "    AND pg_index.indrelid = pg_class.oid\n" +
+            "    AND pg_attribute.attrelid = pg_class.oid\n" +
+            "    AND pg_attribute.attnum = ANY (pg_index.indkey)\n" +
+            ") B ON A.attname = b.attname\n" +
+            "INNER JOIN pg_class C on A.attrelid = C.oid\n" +
+            "INNER JOIN information_schema.columns D on A.attname = D.column_name\n" +
+            "WHERE A.attrelid ='\"%s\"' :: regclass AND A.attnum> 0 AND NOT A.attisdropped AND D.table_name = '%s'\n" +
+            "ORDER BY A.attnum;";
     }
-
 
     @Override
     public String tableName() {
         return "tablename";
     }
 
-
     @Override
     public String tableComment() {
         return "comments";
     }
-
 
     @Override
     public String fieldName() {
         return "name";
     }
 
-
     @Override
     public String fieldType() {
         return "type";
     }
-
 
     @Override
     public String fieldComment() {
         return "comment";
     }
 
-
     @Override
     public String fieldKey() {
         return "key";
     }
 
+    @Override
+    public boolean isKeyIdentity(ResultSet results) throws SQLException {
+        return StringUtils.isNotBlank(results.getString("column_default")) && results.getString("column_default").contains("nextval");
+    }
 }
